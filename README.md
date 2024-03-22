0a511a434c7080a50bcc3b640e6f976ed267e204
Kaneki <37817552+kanekisn@users.noreply.github.com>
2024-03-22 00:18:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WorldApp
+ Test application to figure out who's faster Redis or MySQL using Docker, Hibernate, ObjectMapper.
---
+++ src/main/java/org/worldapp/domain/Continent.java
@@ -0,0 +1,11 @@
+package org.worldapp.domain;
+
+public enum Continent {
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    AFRICA,
+    OCEANIA,
+    ANTARCTICA,
+    SOUTH_AMERICA
+}
